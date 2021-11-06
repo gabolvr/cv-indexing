228c11be6e81eb0b8ad96d9bbe6fb6452f33ab12
Gabriel Oliveira <gabolvrm@gmail.com>
2021-11-06 19:02:52 +0100

    Set up project

---
+++ README.md
@@ -0,0 +1,1 @@
+# cv-indexing
---
+++ src/main/java/com/cvindexing/CvIndexingApplication.java
@@ -0,0 +1,13 @@
+package com.cvindexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvIndexingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvIndexingApplication.class, args);
+	}
+
+}
