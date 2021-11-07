4f809c99c666d0636b0e0e1d36c2c72dc1003cbb
Gabriel Oliveira <gabolvrm@gmail.com>
2021-11-07 15:38:41 +0100

    Add Docx parser and refactor

--- src/main/java/cvindexing/CvController.java
+++ src/main/java/cvindexing/CvController.java
@@ -7,6 +7,12 @@ import java.util.List;
 import java.util.Objects;
 
 import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.elasticsearch.client.ml.inference.preprocessing.Multi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,16 +24,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
 
 @RestController
 @RequestMapping("/api/cvs")
 public class CvController {
     
     private CvSearchService cvSearchService;
+
+    private static final String TYPE_PDF = "application/pdf";
+    private static final String TYPE_DOC = "application/msword";
+    private static final String TYPE_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 
     @Autowired
     public CvController(CvSearchService cvSearchService) {
@@ -45,7 +51,7 @@ public class CvController {
 //        System.out.println("addCv");
 //    }
 
-    @PostMapping(consumes = "application/pdf")
+    @PostMapping(consumes = TYPE_PDF)
     public void addCvPdf(@RequestBody byte[] pdf) throws IOException {
         String pdfContent = getContentFromPdf(new ByteArrayInputStream(pdf));
         cvSearchService.createProductIndex(new Cv(pdfContent));
@@ -53,12 +59,20 @@ public class CvController {
         System.out.println(pdfContent);
     }
 
-    @PostMapping(consumes = "application/msword")
+    @PostMapping(consumes = TYPE_DOC)
     public void addCvDoc(@RequestBody byte[] doc) throws IOException {
-        String pdfContent = getContentFromDoc(new ByteArrayInputStream(doc));
-        cvSearchService.createProductIndex(new Cv(pdfContent));
-        System.out.println("addCvPdf");
-        System.out.println(pdfContent);
+        String docContent = getContentFromDoc(new ByteArrayInputStream(doc));
+        cvSearchService.createProductIndex(new Cv(docContent));
+        System.out.println("addCvDoc");
+        System.out.println(docContent);
+    }
+
+    @PostMapping(consumes = TYPE_DOCX)
+    public void addCvDocx(@RequestBody byte[] docx) throws IOException {
+        String docxContent = getContentFromDocx(new ByteArrayInputStream(docx));
+        cvSearchService.createProductIndex(new Cv(docxContent));
+        System.out.println("addCvDocx");
+        System.out.println(docxContent);
     }
 
     @PostMapping
@@ -70,14 +84,21 @@ public class CvController {
             System.out.println(file.getResource());
             System.out.println(file.getOriginalFilename());
             System.out.println(file.getContentType() == "application/pdf");
-            if (file.getContentType().equals("application/pdf")) {
-                String pdfContent = getContentFromPdf(file.getInputStream());
-                cvSearchService.createProductIndex(new Cv(pdfContent));
-            }
-            if (file.getContentType().equals("application/msword")) {
-                String docContent = getContentFromDoc(file.getInputStream());
-                cvSearchService.createProductIndex(new Cv(docContent));
-                System.out.println(docContent);
+            switch (file.getContentType()) {
+                case TYPE_PDF:
+                    String pdfContent = getContentFromPdf(file.getInputStream());
+                    cvSearchService.createProductIndex(new Cv(pdfContent));
+                    break;
+                case TYPE_DOC:
+                    String docContent = getContentFromDoc(file.getInputStream());
+                    cvSearchService.createProductIndex(new Cv(docContent));
+                    System.out.println(docContent);
+                    break;
+                case TYPE_DOCX:
+                    String docxContent = getContentFromDocx(file.getInputStream());
+                    cvSearchService.createProductIndex(new Cv(docxContent));
+                    System.out.println(docxContent);
+                    break;
             }
         }
     }
@@ -91,6 +112,12 @@ public class CvController {
     private String getContentFromDoc(InputStream doc) throws IOException {
         HWPFDocument document = new HWPFDocument(doc);
         WordExtractor extractor = new WordExtractor(document);
+        return extractor.getText();
+    }
+
+    private String getContentFromDocx(InputStream docx) throws IOException {
+        XWPFDocument document = new XWPFDocument(docx);
+        XWPFWordExtractor extractor = new XWPFWordExtractor(document);
         return extractor.getText();
     }
 }
